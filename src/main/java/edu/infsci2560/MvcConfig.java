1ee60d2ceab4cb9030d2dadf60a2a301c7f03a42
Brian J Kolowitz <bkolowitz@gmail.com>
2017-01-02 16:05:10 +0000

    modify MvcConfig

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
